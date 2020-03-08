7c3ba3bd12e822fa4c0eaf37e39fe97a3ee5e946
Arslanov Rasul <arslanovrasul@MacBook-Pro-Arslanov.local>
2020-03-08 13:30:56 +0300

    bug fixes + readme

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('compile scss', () => {
+gulp.task('compile-scss', () => {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    //.pipe(sourcemaps.write('./'))
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/css'))
 });
 
 gulp.task('watch', () => {
-  watch('./scss/**/*.scss', gulp.series('compile scss'));
+  watch('./scss/**/*.scss', gulp.series('compile-scss'));
 });
